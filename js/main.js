03a20e3522ae603443ecc29b000987d32462fa6a
ArtDaSak <182381184+ArtDaSak@users.noreply.github.com>
2025-04-24 19:58:56 -0500

    feat: :construction: Update index estructure

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 window.addEventListener('scroll', function () {
     const header = document.querySelector('.header');
+    const logo = document.querySelector('.header img');
 
     if (window.scrollY > 10) {
-        header.style.background = 'linear-gradient(to bottom right, rgba(1, 1, 1), rgba(43, 39, 47))';
+        header.style.background = 'linear-gradient(to bottom right, rgba(43, 39, 47), rgba(1, 1, 1))';
+        logo.src = './assets/img/logos/textLogo/textLogoColumbiaBlue.png';
     } else {
-        header.style.background = 'rgba(43, 39, 47, 0.50)'
+        header.style.background = 'rgba(43, 39, 47, 0.50)';
+        logo.src = './assets/img/logos/textLogo/textLogoFireEngineRed.png';
     }
 });
