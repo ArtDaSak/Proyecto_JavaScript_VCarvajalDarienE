98f887f55ffbda53c1e47dd0aa2d94011df82fbb
ArtDaSak <182381184+ArtDaSak@users.noreply.github.com>
2025-05-08 09:00:11 -0500

    style: :construction: Improve Forum Style

--- js/scripts/community.js
+++ js/scripts/community.js
@@ -10,6 +10,19 @@ const loadingElement = document.getElementById('loading');
 const errorElement = document.getElementById('error-message');
 const submitButton = document.getElementById('submit-btn');
 
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('.header');
+    const logo = document.querySelector('.header img');
+
+    if (window.scrollY > 10) {
+        header.style.background = 'linear-gradient(to bottom right, rgba(43, 39, 47), rgba(1, 1, 1))';
+        logo.src = '../assets/img/logos/textLogo/textLogoColumbiaBlue.png';
+    } else {
+        header.style.background = 'rgba(43, 39, 47, 0.50)';
+        logo.src = '../assets/img/logos/textLogo/textLogoTangBlue.png';
+    }
+});
+
 function formatDate(dateString) {
     const options = { 
         year: 'numeric', 
